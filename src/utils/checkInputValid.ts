6a8d017235572b4d8bee71c7f6fc88a022373e5e
bam <juhyun19981129@gmail.com>
2023-09-05 17:38:09 +0900

    일관성 유지

--- src/utils/checkInputValid.ts
+++ src/utils/checkInputValid.ts
@@ -1,9 +1,13 @@
+import {getInputElements} from './getInputElements';
+
 export const checkInputValid = (hour: number, minute: number, second: number): void => {
   if (Number.isNaN(hour) || Number.isNaN(minute) || Number.isNaN(second)) {
+    const [hourInputFormElement, minuteInputFormElement, secondInputFormElement] = getInputElements();
+
     window.alert('유효한 입력값이 아닙니다.');
 
-    (<HTMLInputElement>document.getElementById('hour-input')).value = '';
-    (<HTMLInputElement>document.getElementById('minute-input')).value = '';
-    (<HTMLInputElement>document.getElementById('second-input')).value = '';
+    hourInputFormElement.value = '';
+    minuteInputFormElement.value = '';
+    secondInputFormElement.value = '';
   }
 };
